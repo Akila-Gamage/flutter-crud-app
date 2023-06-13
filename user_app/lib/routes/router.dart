c64ec2c01c502594b20f4c58fb71ca6d9ba42093
Akila-Gamage <akila.20210423@iit.ac.lk>
2023-06-13 23:17:06 +0530

    Update router.dart

--- user_app/lib/routes/router.dart
+++ user_app/lib/routes/router.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:user_app/screens/login.dart';
 import 'package:user_app/screens/signup.dart';
 import 'package:user_app/screens/mainpage.dart';
+import 'package:user_app/screens/secondpage.dart';
 
 class NavigationServices{
   Route<dynamic> generateRoute(RouteSettings settings) {
@@ -12,6 +13,8 @@ class NavigationServices{
       return MaterialPageRoute(builder: (context) => SignUpPage());
     case '/mainpage':
       return MaterialPageRoute(builder: (context) => MainPage());
+    case '/secondpage':
+      return MaterialPageRoute(builder: (context) => SecondPage());
     default:
       return MaterialPageRoute(builder: (context) => LoginPage());
   }
