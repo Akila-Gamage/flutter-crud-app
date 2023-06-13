fc64069c61232b4fa0cbb86fab603802ec14a550
Akila-Gamage <akila.20210423@iit.ac.lk>
2023-06-13 22:12:36 +0530

    Update colors.dart

--- user_app/lib/const/colors.dart
+++ user_app/lib/const/colors.dart
@@ -2,3 +2,5 @@ import 'package:flutter/material.dart';
 
 const backColor = Color(0xFF020202);
 const primaryColor = Color(0xFFFFBD73);
+const textColor = Colors.white;
+const secondaryTextColor = Colors.black;
