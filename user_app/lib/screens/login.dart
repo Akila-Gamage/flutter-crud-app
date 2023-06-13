4688decfa9f92639a03f201d6d0a551be444b446
Akila-Gamage <akila.20210423@iit.ac.lk>
2023-06-13 22:12:51 +0530

    Update login.dart

--- user_app/lib/screens/login.dart
+++ user_app/lib/screens/login.dart
@@ -1,8 +1,10 @@
+import 'dart:ffi';
 import 'package:flutter/material.dart';
-import 'package:animate_do/animate_do.dart';
 import 'package:user_app/const/colors.dart';
 import 'package:user_app/screens/signup.dart';
 import 'package:user_app/routes/router.dart';
+import 'package:form_validator/form_validator.dart';
+import 'package:user_app/services/api_service.dart';
 
 class LoginPage extends StatefulWidget {
   const LoginPage({super.key});
@@ -12,6 +14,21 @@ class LoginPage extends StatefulWidget {
 }
 
 class _LoginPageState extends State<LoginPage> {
+  final formkey = GlobalKey<FormState>();
+  final scaffoldKey = new GlobalKey();
+  final TextEditingController usernameController = TextEditingController();
+  final TextEditingController passwordController = TextEditingController();
+
+  Future<void> login() async {
+   final isLoggedIn = await loginIn(usernameController.text, passwordController.text);
+
+   if (isLoggedIn) {
+    Navigator.pushNamed(context, '/mainpage');
+   }else {
+    print("Invalid credentials");
+   }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -34,116 +51,122 @@ class _LoginPageState extends State<LoginPage> {
             flex: 4,
             child: Padding(
               padding: const EdgeInsets.symmetric(horizontal: 16),
-              child: Column(
-                // mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: <Widget>[
-                  Padding(
-                    padding:
-                        EdgeInsets.only(left: 0, right: 0, top: 10, bottom: 10),
-                    child: Row(
-                      children: <Widget>[
-                        Text(
-                          "SIGN IN",
-                          style: Theme.of(context).textTheme.displayMedium,
-                        ),
-                      ],
-                    ),
-                  ),
-                  SizedBox(height: 45,),
-                  Padding(
-                    padding: const EdgeInsets.only(left: 15, right: 15, top: 15, bottom: 0),
-                    child: Row(
-                      crossAxisAlignment: CrossAxisAlignment.center,
-                      children: <Widget>[
-                        const Padding(
-                          padding: EdgeInsets.only(right: 16),
-                          child: Icon(
-                            Icons.person_3_outlined,
-                            color: primaryColor,
+              child: Form(
+                key: formkey,
+                child: Column(
+                  children: <Widget>[
+                    Padding(
+                      padding:
+                          EdgeInsets.only(left: 0, right: 0, top: 10, bottom: 10),
+                      child: Row(
+                        children: <Widget>[
+                          Text(
+                            "SIGN IN",
+                            style: Theme.of(context).textTheme.displayMedium,
                           ),
-                        ),
-                        Expanded(
-                          child: Text(
-                            "Username",
-                            style: Theme.of(context).textTheme.labelSmall,
-                          ),
-                        ),
-                      ],
+                        ],
+                      ),
                     ),
-                  ),
-                  const Padding(
-                    padding: EdgeInsets.only(left: 15, right: 15, top: 0, bottom: 0),
-                    child: TextField(
-                      decoration:
-                          InputDecoration(hintText: "Enter your username"),
-                    ),
-                  ),
-                  SizedBox(height: 35,),
-                  Padding(
-                    padding: const EdgeInsets.only(left: 15, right: 15, top: 0, bottom: 0),
-                    child: Row(
-                      crossAxisAlignment: CrossAxisAlignment.center,
-                      children: <Widget>[
-                        const Padding(
-                          padding: EdgeInsets.only(right: 16),
-                          child: Icon(
-                            Icons.lock,
-                            color: primaryColor,
-                          ),
-                        ),
-                        Expanded(
-                          child: Text(
-                            "Password",
-                            style: Theme.of(context).textTheme.labelSmall,
-                          ),
-                        ),
-                      ],
-                    ),
-                  ),
-                  const Padding(
-                    padding: EdgeInsets.only(left: 15, right: 15, top: 0, bottom: 15),
-                    child: TextField(
-                      decoration:
-                          InputDecoration(hintText: "Enter your password"),
-                    ),
-                  ),
-                  SizedBox(height: 45,),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                    children: [
-                      Container(
-                        width: 150,
-                        child: OutlinedButton(
-                          onPressed: () =>
-                              Navigator.pushNamed(context, '/signup'),
-                          child: Text('Sign Up'),
-                          style: OutlinedButton.styleFrom(
-                            foregroundColor: primaryColor,
-                            side: BorderSide(
-                              width: 1,
+                    SizedBox(height: 45,),
+                    Padding(
+                      padding: const EdgeInsets.only(left: 15, right: 15, top: 15, bottom: 0),
+                      child: Row(
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: <Widget>[
+                          const Padding(
+                            padding: EdgeInsets.only(right: 16),
+                            child: Icon(
+                              Icons.person_3_outlined,
                               color: primaryColor,
-                              style: BorderStyle.solid,
                             ),
                           ),
-                        ),
+                          Expanded(
+                            child: Text(
+                              "Username",
+                              style: Theme.of(context).textTheme.labelSmall,
+                            ),
+                          ),
+                        ],
                       ),
-                      Container(
-                        width: 150,
-                        child: ElevatedButton(
-                          onPressed: () =>
-                              Navigator.pushNamed(context, '/mainpage'),
-                          child: Text('Sign In'),
-                          style: ElevatedButton.styleFrom(
-                              backgroundColor: primaryColor,
+                    ),
+                    Padding(
+                      padding: EdgeInsets.only(left: 15, right: 15, top: 0, bottom: 0),
+                      child: TextFormField(
+                        controller: usernameController,
+                        decoration:
+                            InputDecoration(hintText: "Enter your username"),
+                      ),
+                    ),
+                    SizedBox(height: 35,),
+                    Padding(
+                      padding: const EdgeInsets.only(left: 15, right: 15, top: 0, bottom: 0),
+                      child: Row(
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: <Widget>[
+                          const Padding(
+                            padding: EdgeInsets.only(right: 16),
+                            child: Icon(
+                              Icons.lock,
+                              color: primaryColor,
+                            ),
+                          ),
+                          Expanded(
+                            child: Text(
+                              "Password",
+                              style: Theme.of(context).textTheme.labelSmall,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                    Padding(
+                      padding: EdgeInsets.only(left: 15, right: 15, top: 0, bottom: 15),
+                      child: TextFormField(
+                        controller: passwordController,
+                        decoration:
+                            InputDecoration(hintText: "Enter your password"),
+                      ),
+                    ),
+                    SizedBox(height: 45,),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: [
+                        Container(
+                          width: 150,
+                          child: OutlinedButton(
+                            onPressed: () =>
+                                Navigator.pushNamed(context, '/signup'),
+                            child: Text('Sign Up'),
+                            style: OutlinedButton.styleFrom(
+                              foregroundColor: primaryColor,
                               side: BorderSide(
                                 width: 1,
                                 color: primaryColor,
-                              )),
+                                style: BorderStyle.solid,
+                              ),
+                            ),
+                          ),
                         ),
-                      )
-                    ],
-                  ),
-                ],
+                        Container(
+                          width: 150,
+                          child: ElevatedButton(
+                            onPressed: login,
+                            // onPressed: () =>
+                                // Navigator.pushNamed(context, '/mainpage'),
+                            child: Text('Sign In'),
+                            style: ElevatedButton.styleFrom(
+                                backgroundColor: primaryColor,
+                                foregroundColor: backColor,
+                                side: BorderSide(
+                                  width: 1,
+                                  color: primaryColor,
+                                )),
+                          ),
+                        )
+                      ],
+                    ),
+                  ],
+                ),
               ),
             ),
           )
