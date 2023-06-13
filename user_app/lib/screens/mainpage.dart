f4ff16450457c92d1a1f0719533456c668c4ee9a
Akila-Gamage <akila.20210423@iit.ac.lk>
2023-06-13 23:16:59 +0530

    Update mainpage.dart

--- user_app/lib/screens/mainpage.dart
+++ user_app/lib/screens/mainpage.dart
@@ -15,77 +15,89 @@ class _MainPageState extends State<MainPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        drawer: NavBar(),
-        appBar: AppBar(
-          title: Text("Home"),
+      drawer: NavBar(),
+      appBar: AppBar(
+        title: Text("List View - User data"),
+      ),
+      body: Container(
+        color: backColor,
+        padding: const EdgeInsets.all(8),
+        child: FutureBuilder(
+          future: getUsers(),
+          builder: (context, snapshot) {
+            if (snapshot.hasData) {
+              List<User> users = snapshot.data as List<User>;
+              return ListView.builder(
+                  itemCount: users.length,
+                  itemBuilder: (context, index) {
+                    return Card(
+                      color: Colors.grey.shade800,
+                      margin: const EdgeInsets.only(
+                          bottom: 20, top: 20, right: 10, left: 10),
+                      child: ListTile(
+                          title: Text(users[index].name,
+                              style: const TextStyle(color: textColor)),
+                          subtitle: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              const SizedBox(
+                                height: 20,
+                              ),
+                              Text(
+                                users[index].email,
+                                style: const TextStyle(color: textColor),
+                              ),
+                              const SizedBox(
+                                height: 5,
+                              ),
+                              Text(
+                                users[index].password,
+                                style: const TextStyle(color: textColor),
+                              ),
+                              const SizedBox(
+                                height: 5,
+                              ),
+                            ],
+                          ),
+                          trailing: Row(
+                            mainAxisSize: MainAxisSize.min,
+                            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                            children: [
+                              ElevatedButton(
+                                onPressed: null,
+                                style: ButtonStyle(
+                                  backgroundColor:
+                                      MaterialStateProperty.all(primaryColor),
+                                ),
+                                child: const Text("Edit",
+                                    style:
+                                        TextStyle(color: secondaryTextColor)),
+                              ),
+                              const SizedBox(
+                                width: 20,
+                              ),
+                              ElevatedButton(
+                                onPressed: null,
+                                style: ButtonStyle(
+                                  backgroundColor:
+                                      MaterialStateProperty.all(Colors.red),
+                                ),
+                                child: const Text("Delete",
+                                    style: TextStyle(color: Colors.white)),
+                              ),
+                            ],
+                          )),
+                    );
+                  });
+            }
+            if (snapshot.hasError) {
+              print(snapshot.error.toString());
+              return Text('error');
+            }
+            return CircularProgressIndicator();
+          },
         ),
-        body: Container(
-          color: backColor,
-          padding: const EdgeInsets.all(8),
-          child: FutureBuilder(
-            future: getUsers(),
-            builder: (context, snapshot) {
-              if (snapshot.hasData) {
-                List<User> users = snapshot.data as List<User>;
-                return ListView.builder(
-                    itemCount: users.length,
-                    itemBuilder: (context, index) {
-                      return Card(
-                        margin: EdgeInsets.only(bottom: 20, top: 20, right: 10, left: 10),
-                        child: ListTile(
-                            title: Text(users[index].name,
-                                style: TextStyle(color: textColor)),
-                            subtitle: Column(
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              children: [
-                                SizedBox(height: 20,),
-                                Text(
-                                  users[index].email,
-                                  style: TextStyle(color: textColor),
-                                ),
-                                Text(
-                                  users[index].password,
-                                  style: TextStyle(color: textColor),
-                                )
-                              ],
-                            ),
-                            
-                            trailing: Row(
-                              mainAxisSize: MainAxisSize.min,
-                              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                              children: [
-                                ElevatedButton(
-                                  onPressed: null,
-                                  child: Text("Edit",
-                                      style: TextStyle(color: secondaryTextColor)),
-                                  style: ButtonStyle(
-                                    backgroundColor:
-                                        MaterialStateProperty.all(primaryColor),
-                                  ),
-                                ),
-                                SizedBox(width: 20,),
-                                ElevatedButton(
-                                  onPressed: null,
-                                  child: Text("Delete",
-                                      style: TextStyle(color: Colors.white)),
-                                  style: ButtonStyle(
-                                    backgroundColor:
-                                        MaterialStateProperty.all(Colors.red),
-                                  ),
-                                ),
-                              ],
-                            )),
-                        color: Colors.grey.shade800,
-                      );
-                    });
-              }
-              if (snapshot.hasError) {
-                print(snapshot.error.toString());
-                return Text('error');
-              }
-              return CircularProgressIndicator();
-            },
-          ),
-        ));
+      ),
+    );
   }
 }
